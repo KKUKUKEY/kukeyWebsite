288ba6a3ce88437dc61b9d6fcd084919c4d7e109
soooonho <tnsgh2120@gmail.com>
2026-09-11 19:33:30 +0900

    footer-load수정

--- app/api/footer-load/route.ts
+++ app/api/footer-load/route.ts
@@ -1,16 +1,20 @@
 import pool from "../../lib/db";
-
+import type { RowDataPacket } from "mysql2";
+interface SiteSettingRow extends RowDataPacket {
+  instagramUrl: string | null;
+  githubUrl: string | null;
+  representativeEmail: string | null;
+}
 export async function GET() {
   try {
-    const [rows] = await pool.query(
+    const [rows] = await pool.query<SiteSettingRow[]>(
       "SELECT instagramUrl, githubUrl, representativeEmail FROM site_setting",
     );
-
     return Response.json(
       {
         result: true,
         message: "데이터 로드 성공",
-        data: rows,
+        data: rows[0],
       },
       { status: 200 },
     );
